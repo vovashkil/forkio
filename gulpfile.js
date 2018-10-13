e5476dca5d642aa9ecd329162893a0cd6b354eb4
Vova Shkil <vshkil@tucows.net>
2018-10-13 09:43:13 -0400

    more or less final gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,109 @@
-'use strict'
+"use strict";
 
-var gulp = require('gulp')
-var minify = require('gulp-uglify')
-var browserSync = require('browser-sync').create()
-var sass = require('gulp-sass')
-var clean = require('gulp-clean')
-var concat = require('gulp-concat')
-var rename = require('gulp-rename')
-var imagemin = require('gulp-imagemin')
-var eslint = require('gulp-eslint')
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require("gulp");
+var minify = require("gulp-uglify");
+var browserSync = require("browser-sync").create();
+var sass = require("gulp-sass");
+var clean = require("gulp-clean");
+var concat = require("gulp-concat");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var eslint = require("gulp-eslint");
+var sourcemaps = require("gulp-sourcemaps");
+var runSequence = require("run-sequence");
 
-gulp.task('lint', () => {
-    gulp.src(['**/*.js','!node_modules/**'])
+gulp.task("lint", function(){
+    gulp.src(["**/*.js","!node_modules/**"])
         .pipe(eslint({
             rules: {
                 "semi": ["error", "always"],
                 "quotes": ["error", "double"]
             },
             globals: [
-                'jQuery',
-                '$'
+                "jQuery",
+                "$"
             ],
             envs: [
-                'browser'
+                "browser"
             ]
         }))
-        .pipe(eslint.formatEach('compact', process.stderr));
+        .pipe(eslint.formatEach("compact", process.stderr));
 });
 
 
-gulp.task('img', function(){
-    return gulp.src('./src/img/*')
+gulp.task("img", function(){
+    return gulp.src("./src/img/*")
         .pipe(imagemin({
             interlaced: true,
             progressive: true,
             svgoPlugins: [{removeViewBox: false}]
         }))
-        .pipe(gulp.dest('./build/img'))
-})
+        .pipe(gulp.dest("./dist/img"));
+});
 
-gulp.task('minify', function(){
-    return gulp.src('./src/js/*.js')
-        .pipe(concat('bundle.js'))
-        .pipe(gulp.dest('./build'))
+gulp.task("minify", function(){
+    return gulp.src("./src/js/*.js")
+        .pipe(concat("bundle.js"))
+        .pipe(gulp.dest("./dist"))
         .pipe(minify())
-        .pipe(rename('bundle.min.js'))
-        .pipe(gulp.dest('./build'))
-})
+        .pipe(rename("bundle.min.js"))
+        .pipe(gulp.dest("./dist"));
+});
 
-gulp.task('clean', function(){
-    return gulp.src('./build', {read: false})
-        .pipe(clean())
-})
+gulp.task("clean", function(){
+    return gulp.src("./dist", {read: false})
+        .pipe(clean());
+});
 
-gulp.task('serve', ['clean'], function (){
-    browserSync.init({
-        server: "./build/"
-    })
-    gulp.src('./src/index.html').pipe(gulp.dest('./build/'));
-    gulp.watch('./src/scss/*.scss',['sass']).on('change', browserSync.reload);
-    gulp.watch('./src/js/*.js',['minify']).on('change', browserSync.reload);
-    gulp.watch('./build/index.html').on('change', browserSync.reload)
-})
+gulp.task("copyFonts", function(){
+    return gulp.src("./src/fonts/**/*")
+        .pipe(gulp.dest("./dist/fonts"));
+});
 
-gulp.task('sass', function(){
-    return gulp.src('./src/scss/*.scss')
+
+gulp.task("serve", function (){
+    runSequence("clean", ["sass","copyFonts"], function(){
+        browserSync.init({
+            server: "./dist/"
+        });
+
+        gulp.src("./src/index.html").pipe(gulp.dest("./dist/"));
+        gulp.watch("./src/scss/*.scss",["sass"]).on("change", browserSync.reload);
+        gulp.watch("./src/index.html").on("change", function(){
+            return gulp.src("./src/index.html").pipe(gulp.dest("./dist/"));
+        });
+        gulp.watch("./dist/index.html").on("change", browserSync.reload);
+    });
+});
+
+gulp.task("sass", function(){
+    return gulp.src("./src/scss/*.scss")
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./build/css'))
-})
+        .pipe(gulp.dest("./dist/css"));
+});
 
 
 
-gulp.task('clean', function () {
-    return gulp.src('./build', {read: false})
-        .pipe(clean())
-})
+gulp.task("clean", function () {
+    return gulp.src("./dist", {read: false})
+        .pipe(clean());
+});
 
 
 
-gulp.task('img', function(){
-    return gulp.src('./src/img/**/*')
+gulp.task("img", function(){
+    return gulp.src("./src/img/**/*")
         .pipe(imagemin({
                 interlaced: true,
                 progressive: true,
                 svgoPlugins: [{removeViewBox:false}],
             })
         )
-        .pipe(gulp.dest('./build/img'))
-})
+        .pipe(gulp.dest("./dist/img"));
+});
 
-gulp.task('default', ['serve', 'lint'], function(){
-    console.log('=== ALL DONE ===')
-})
+gulp.task("default", ["serve", "lint"], function(){
+    console.log("=== ALL DONE ===");
+});
